7ac3b3c713a00f9edc3941fa4efa1c40573102ee
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-08 16:25:46 +0700

    receive/send + fb_access_token

--- server.js
+++ server.js
@@ -28,6 +28,47 @@ app.get('/webhook', function(req, res) {
     res.send('Error, wrong validation token');
 });
 
+// Xử lý khi có người nhắn tin cho bot
+app.post('/webhook', function(req, res) {
+    var entries = req.body.entry;
+    for (var entry of entries) {
+        var messaging = entry.messaging;
+        for (var message of messaging) {
+            var senderId = message.sender.id;
+            if (message.message) {
+                // If user send text
+                if (message.message.text) {
+                    var text = message.message.text;
+                    console.log(text); // In tin nhắn người dùng
+                    sendMessage(senderId, "Tui là bot đây: " + text);
+                }
+            }
+        }
+    }
+
+    res.status(200).send("OK");
+});
+
+
+// Gửi thông tin tới REST API để trả lời
+function sendMessage(senderId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: "EAAJUPFUQ9b4BAO6F6y64NWaOZCE7yI7QkZBR1wiHDqKqbyVP6vvwvrsnKZCft8esyKJhuQjJAbkcYTh7XVmR20dXakrFRwk0NlZAhdre9CdRP9lZCZBnGxOndUbFfKLPvXIaejLv0vuZCCb3GzgqnHHJ1acbgBnYxZBaIFDS2DkfmbjTZA0PZBpWk7gtIE5WdxY3FJd27OqHYaIAZDZD",
+        },
+        method: 'POST',
+        json: {
+            recipient: {
+                id: senderId
+            },
+            message: {
+                text: message
+            },
+        }
+    });
+}
+
 app.set('port', process.env.PORT || 8080);
 app.set('ip', process.env.IP);
 
