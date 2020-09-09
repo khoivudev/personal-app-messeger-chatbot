3527b3e2afdd317dd9d1d8ff2f29b6e3a2d3f52e
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-09 07:52:20 +0700

    update server #1

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/webhook', function(req, res) {
                 if (message.message.text) {
                     var text = message.message.text;
                     console.log(text); // In tin nhắn người dùng
-                    sendMessage(senderId, "Tui là bot đây: " + text);
+                    sendMessage(senderId, "Hello.Nice to meet you. I'm assistant of Mr.Khoi. Can I help you?");
                 }
             }
         }
