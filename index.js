c9de9392b7e11c9ac4e93dab685821eecbe12a85
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-09 16:12:14 +0700

    fix variable

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const messageWebhook = require('./message-webhook');
 const verifyWebhook = require('./verify-webhook');
 
 const app = express();
+var server = http.createServer(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +15,7 @@ app.get('/', (req, res) => {
     res.send("Home page. Server running okay.");
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
-app.listen(app.get('port'), app.get('ip'), () => console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port')));
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP);
+
+server.listen(app.get('port'), app.get('ip'), () => console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port')));
