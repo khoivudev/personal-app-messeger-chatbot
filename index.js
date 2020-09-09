d3d216552b9536e2a2d9eefc606b9c577f560665
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-09 16:14:56 +0700

    fix bug

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const messageWebhook = require('./message-webhook');
 const verifyWebhook = require('./verify-webhook');
-
+var http = require('http');
 const app = express();
 var server = http.createServer(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', verifyWebhook);
-app.post('/', messageWebhook);
 app.get('/', (req, res) => {
     res.send("Home page. Server running okay.");
 });
+app.get('/webhook', verifyWebhook);
+app.post('/webhook', messageWebhook);
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP);
